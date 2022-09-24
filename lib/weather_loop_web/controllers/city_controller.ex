6e5da98f0a8dbbbe07ad1affb73ed8e397cfd24d
Ryan Murphy <murjax@gmail.com>
2022-09-24 14:39:27 -0400

    Refactor current user logic in controller

--- lib/weather_loop_web/controllers/city_controller.ex
+++ lib/weather_loop_web/controllers/city_controller.ex
@@ -6,11 +6,7 @@ defmodule WeatherLoopWeb.CityController do
   alias WeatherLoop.Cities
   alias WeatherLoop.Cities.City
 
-  def action(%Plug.Conn{assigns: %{current_user: current_user}} = conn, _opts) do
-    apply(__MODULE__, action_name(conn), [conn, conn.params, current_user])
-  end
-
-  def index(conn, _params, current_user) do
+  def index(conn, _params) do
     city_query = from city in City, as: :city,
       left_join: snapshot in subquery(
         from snapshot in WeatherSnapshot,
@@ -20,20 +16,20 @@ defmodule WeatherLoopWeb.CityController do
       ), on: snapshot.city_id == city.id,
       distinct: city.id,
       select: %{id: city.id, name: city.name, state: city.state, temperature: snapshot.temperature, weather_title: snapshot.weather_title},
-      where: city.user_id == ^current_user.id
+      where: city.user_id == ^conn.assigns.current_user.id
 
     cities = WeatherLoop.Repo.all(city_query)
 
     render(conn, "index.html", cities: cities)
   end
 
-  def new(conn, _params, _current_user) do
+  def new(conn, _params) do
     changeset = City.changeset(%City{})
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, %{"city" => city_params}, current_user) do
-    city_params = Map.put(city_params, "user_id", current_user.id)
+  def create(conn, %{"city" => city_params}) do
+    city_params = Map.put(city_params, "user_id", conn.assigns.current_user.id)
     case Cities.create_city(city_params) do
       {:ok, city} ->
         WeatherLoop.WeatherApi.capture_snapshots(city)
@@ -47,13 +43,13 @@ defmodule WeatherLoopWeb.CityController do
     end
   end
 
-  def edit(conn, %{"id" => id}, _current_user) do
+  def edit(conn, %{"id" => id}) do
     city = Cities.get_city!(id)
     changeset = City.changeset(city)
     render(conn, "edit.html", city: city, changeset: changeset)
   end
 
-  def update(conn, %{"id" => id, "city" => city_params}, current_user) do
+  def update(conn, %{"id" => id, "city" => city_params}) do
     city = Cities.get_city!(id)
 
     case Cities.update_city(city, city_params) do
@@ -69,7 +65,7 @@ defmodule WeatherLoopWeb.CityController do
     end
   end
 
-  def delete(conn, %{"id" => id}, _current_user) do
+  def delete(conn, %{"id" => id}) do
     city = Cities.get_city!(id)
 
     WeatherSnapshots.delete_snapshots_for_city_id(id)
